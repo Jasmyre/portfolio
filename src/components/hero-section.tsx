5c1002abcd500909d1ad35386fcd73b96f7fd93c
Jasmyre <lanuzajasmyreandrei@gmail.com>
2025-08-02 21:40:15 +0800

    Experimental(ui): Add sections

---
+++ src/components/hero-section.tsx
@@ -0,0 +1,193 @@
+import { AnimatedGroup } from "@/components/ui/animated-group";
+import { Button } from "@/components/ui/button";
+import { TextEffect } from "@/components/ui/text-effect";
+import { ArrowDown } from "lucide-react";
+import Link from "next/link";
+
+export default function HeroSection() {
+  return (
+    <>
+      {/* <HeroHeader /> */}
+      <main className="relative flex min-h-[90vh] items-center justify-center overflow-hidden lg:min-h-screen">
+        <AnimatedGroup
+          variants={{
+            container: {
+              visible: {
+                transition: {
+                  staggerChildren: 0.05,
+                  delayChildren: 1,
+                },
+              },
+            },
+            item: {
+              hidden: {
+                opacity: 0,
+                y: 20,
+              },
+              visible: {
+                opacity: 1,
+                y: 0,
+                transition: {
+                  type: "spring",
+                  bounce: 0.3,
+                  duration: 2,
+                },
+              },
+            },
+          }}
+          className="absolute bottom-10 animate-bounce"
+        >
+          <Button
+            size={"icon"}
+            variant={"outline"}
+            className="rounded-full opacity-75"
+          >
+            <ArrowDown />
+          </Button>
+        </AnimatedGroup>
+        <div
+          aria-hidden
+          className="absolute inset-0 isolate hidden opacity-65 contain-strict lg:block"
+        >
+          <div className="absolute top-0 left-0 h-320 w-140 -translate-y-87.5 -rotate-45 rounded-full bg-[radial-gradient(68.54%_68.72%_at_55.02%_31.46%,hsla(0,0%,85%,.08)_0,hsla(0,0%,55%,.02)_50%,hsla(0,0%,45%,0)_80%)]" />
+          <div className="absolute top-0 left-0 h-320 w-60 [translate:5%_-50%] -rotate-45 rounded-full bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.06)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
+          <div className="absolute top-0 left-0 h-320 w-60 -translate-y-87.5 -rotate-45 bg-[radial-gradient(50%_50%_at_50%_50%,hsla(0,0%,85%,.04)_0,hsla(0,0%,45%,.02)_80%,transparent_100%)]" />
+        </div>
+        <section className="flex items-center justify-center">
+          <div className="relative flex items-center justify-center">
+            <div className="absolute inset-0 -z-10 size-full [background:radial-gradient(125%_125%_at_50%_100%,transparent_0%,var(--color-background)_75%)]"></div>
+            <div className="mx-auto max-w-7xl px-6">
+              <div className="text-center sm:mx-auto lg:mt-0 lg:mr-auto">
+                <TextEffect
+                  preset="fade-in-blur"
+                  speedSegment={0.3}
+                  as="h1"
+                  className="text-4xl sm:text-6xl text-balance md:text-7xl lg:text-6xl xl:text-[5.25rem]"
+                >
+                  No tech skills? No problem. I&apos;ll build the site for you.
+                </TextEffect>
+                <TextEffect
+                  per="line"
+                  preset="fade-in-blur"
+                  speedSegment={0.275}
+                  delay={0.5}
+                  as="p"
+                  className="text-muted-foreground mx-auto mt-8 max-w-2xl text-lg  md:text-pretty"
+                >
+                  You don’t need to know how websites work—I’ll handle the
+                  details and build something that fits exactly what you need
+                </TextEffect>
+
+                <AnimatedGroup
+                  variants={{
+                    container: {
+                      visible: {
+                        transition: {
+                          staggerChildren: 0.05,
+                          delayChildren: 0.75,
+                        },
+                      },
+                    },
+                    item: {
+                      hidden: {
+                        opacity: 0,
+                        y: 20,
+                      },
+                      visible: {
+                        opacity: 1,
+                        y: 0,
+                        transition: {
+                          type: "spring",
+                          bounce: 0.3,
+                          duration: 2,
+                        },
+                      },
+                    },
+                  }}
+                  className="mt-12 flex flex-col items-center justify-center gap-2 md:flex-row"
+                >
+                  <div
+                    key={1}
+                    className="bg-foreground/10 rounded-[calc(var(--radius-xl)+0.125rem)] border p-0.5"
+                  >
+                    <Button
+                      asChild
+                      size="lg"
+                      className="rounded-xl px-5 text-base max-md:w-full"
+                    >
+                      <Link href="#link">
+                        <span className="text-nowrap">View portfolio</span>
+                      </Link>
+                    </Button>
+                  </div>
+                  <Button
+                    key={2}
+                    asChild
+                    size="lg"
+                    variant="outline"
+                    className="h-10.5 rounded-xl px-5 max-md:w-full"
+                  >
+                    <Link href="#link">
+                      <span className="text-nowrap">Contact me</span>
+                    </Link>
+                  </Button>
+                </AnimatedGroup>
+              </div>
+            </div>
+
+            {/* <AnimatedGroup
+              variants={{
+                container: {
+                  visible: {
+                    transition: {
+                      staggerChildren: 0.05,
+                      delayChildren: 0.75,
+                    },
+                  },
+                },
+                item: {
+                  hidden: {
+                    opacity: 0,
+                    y: 20,
+                  },
+                  visible: {
+                    opacity: 1,
+                    y: 0,
+                    transition: {
+                      type: "spring",
+                      bounce: 0.3,
+                      duration: 2,
+                    },
+                  },
+                },
+              }}
+            >
+              <div className="relative mt-8 -mr-56 overflow-hidden px-2 sm:mt-12 sm:mr-0 md:mt-20">
+                <div
+                  aria-hidden
+                  className="to-background absolute inset-0 z-10 bg-linear-to-b from-transparent from-35%"
+                />
+                <div className="ring-background bg-background relative mx-auto max-w-6xl overflow-hidden rounded-2xl border p-4 shadow-lg ring-1 inset-shadow-2xs shadow-zinc-950/15 dark:inset-shadow-white/20">
+                  <Image
+                    className="bg-background relative hidden aspect-15/8 rounded-2xl dark:block"
+                    src="/ictquest.png"
+                    alt="app screen"
+                    width="2700"
+                    height="1540"
+                  />
+                  <Image
+                    className="border-border/25 relative z-2 aspect-15/8 rounded-2xl border dark:hidden"
+                    src="/mail2-light.png"
+                    alt="app screen"
+                    width="2700"
+                    height="1540"
+                  />
+                </div>
+              </div>
+            </AnimatedGroup> */}
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
