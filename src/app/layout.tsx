8927a9068b69cb8524af4e48bcb53142bcb49147
Jasmyre <lanuzajasmyreandrei@gmail.com>
2025-08-14 15:59:12 +0800

    refactor: Remove commented code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,8 +26,8 @@ export default function RootLayout({
         <TRPCReactProvider>
           <ThemeProvider
             attribute="class"
-            defaultTheme="light"
-            // enableSystem
+            defaultTheme="dark"
+            enableSystem
             disableTransitionOnChange
           >
             {children}
