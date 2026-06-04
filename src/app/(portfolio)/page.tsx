3c3c6f10d6ab27fd2e66e260d047f06c79fd744f
Jasmyre <lanuzajasmyreandrei@gmail.com>
2026-06-04 17:45:28 +0800

    Refactor HomePage component: clean up commented code and adjust main section height

--- src/app/(portfolio)/page.tsx
+++ src/app/(portfolio)/page.tsx
@@ -8,21 +8,14 @@ import { ServicesSection } from "@/components/pages/portfolio/service-section";
 import { HydrateClient } from "@/trpc/server";
 
 export default async function HomePage() {
-  // const hello = await api.post.hello({ text: "from tRPC Test" });
-
-  // await api.ai.test({text: "What is the meaning of life?"});
-
-  // void api.post.getLatest.prefetch();
-
   return (
     <HydrateClient>
       <div className="relative">
-        <main className="min-h-[100vh] w-full" id="home">
+        <main className="min-h-screen w-full" id="home">
           <HeroSection />
           <ContentSection />
           <ServicesSection />
           <ProjectsSection label={"Highlights"} limit={3} />
-          {/* <WallOfLoveSection /> */}
           <CTASection />
           <ContactSection />
         </main>
